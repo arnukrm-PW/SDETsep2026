4d96d8611010de8e6fcec6f37d6dd6b0a46152d7
arun <arnukrm0@gmail.com>
2026-09-20 18:34:34 +0530

    updated script

--- tests/Integration/login.spec.js
+++ tests/Integration/login.spec.js
@@ -62,7 +62,7 @@ test("login to SD",async ({page})=>{
    await  expect(page).toHaveURL(/inventory/)
    await expect(page.locator(productLocators.openMenu)).toBeVisible()
    await page.waitForTimeout(2000)
-   //await page.pause() all clean setup
+   //await page.pause() 
 })
     
 test("About page",async ({page})=>{
